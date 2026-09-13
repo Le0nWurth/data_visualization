bd9a38dab27b80c8845fdbce558ecb6b93195643
Leon <1leon012@gmail.com>
2026-09-13 12:17:36 +0200

    scatterplots

--- src/analysis/figures.py
+++ src/analysis/figures.py
@@ -20,3 +20,14 @@ def make_correlation_heatmap(correlation_matrix: pd.DataFrame) -> object:
     )
 
     return fig
+
+
+def make_scatter_plot(data: pd.DataFrame, x_col: str, y_col: str, hue_col="") -> object:
+    sns.set_theme(style="darkgrid")
+    fig, ax = plt.subplots(figsize=(10, 8))
+    if hue_col != "":
+        sns.scatterplot(data=data, x=x_col, y=y_col, hue=hue_col, ax=ax)
+    else:
+        sns.scatterplot(data=data, x=x_col, y=y_col, ax=ax)
+
+    return fig
--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 import streamlit as st
 
-from analysis.figures import make_correlation_heatmap
+from analysis.figures import make_correlation_heatmap, make_scatter_plot
 from data.data_import import import_sleep_data
 
 
@@ -10,16 +10,54 @@ def main():
     corr_mat = data.corr(numeric_only=True)  # print_correlation_matrix(data)
     corr_fig = make_correlation_heatmap(corr_mat)
 
+    # Scatterplots
+    sleep_efficiency_deep_sleep = make_scatter_plot(
+        data, "Sleep efficiency", "Deep sleep percentage", "Exercise frequency"
+    )
+    sleep_efficiency_light_sleep = make_scatter_plot(
+        data, "Sleep efficiency", "Light sleep percentage"
+    )
+    sleep_efficiency_awakenings = make_scatter_plot(
+        data, "Sleep efficiency", "Awakenings"
+    )
+    sleep_efficiency_caffeine = make_scatter_plot(
+        data, "Sleep efficiency", "Caffeine consumption"
+    )
+    sleep_efficiency_alcohol = make_scatter_plot(
+        data, "Sleep efficiency", "Alcohol consumption"
+    )
+    sleep_efficiency_exercise = make_scatter_plot(
+        data, "Sleep efficiency", "Exercise frequency"
+    )
+
+    # UI
+
     st.set_page_config(page_title="Data-Vis", layout="wide")
     st.title("Data")
-
     st.subheader("Raw Data")
     st.dataframe(data, hide_index=True)
+    col1, col2, col3 = st.columns(3)
 
-    st.subheader("Correlation Matrix")
-    st.dataframe(corr_mat)
+    col1.subheader("Correlation Heatmap")
+    col1.pyplot(corr_fig)
 
-    st.pyplot(corr_fig)
+    col2.subheader("Sleep efficency - Deep sleep - Exercise")
+    col2.pyplot(sleep_efficiency_deep_sleep)
+
+    col3.subheader("")
+    col3.pyplot(sleep_efficiency_light_sleep)
+
+    col1.subheader("")
+    col1.pyplot(sleep_efficiency_awakenings)
+
+    col2.subheader("")
+    col2.pyplot(sleep_efficiency_alcohol)
+
+    col3.subheader("")
+    col3.pyplot(sleep_efficiency_exercise)
+
+    col1.subheader("")
+    col1.pyplot(sleep_efficiency_caffeine)
 
 
 if __name__ == "__main__":
